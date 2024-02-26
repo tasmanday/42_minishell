d2b021de8638222944c8e755f28f1ad470572409
Tasman Day <122511599+tasmanday@users.noreply.github.com>
2024-02-26 23:21:27 +1030

    added comments

--- src/parse/handle_meta_chars.c
+++ src/parse/handle_meta_chars.c
@@ -12,6 +12,23 @@
 
 #include "../../inc/minishell.h"
 
+/*
+	**** ALLOCATES MEMORY ****
+	memory is allocated for a linked list node in safe_new_token_node function.
+
+	Summary
+	adds redirection tokens to msh->tokens based on the current character in
+	the input string.
+
+	Inputs
+	[t_msh *] msh: a pointer to the minishell main structure.
+	[char *] str: the input string to be processed.
+	[int *] i: a pointer to the index variable.
+
+	Outputs
+	none. the function modifies the minishell structure by adding tokens to the
+	msh->tokens linked list.
+*/
 static void	add_redirection_tokens(t_msh *msh, char *str, int *i)
 {
 	t_list	*new_node;
@@ -37,7 +54,23 @@ static void	add_redirection_tokens(t_msh *msh, char *str, int *i)
 		*i += ft_strlen((char *)new_node->data);
 	}
 }
+/*
+	**** ALLOCATES MEMORY ****
+	memory is allocated for a linked list node in safe_new_token_node function.
 
+	Summary
+	adds a pipe token to msh->tokens if the current character in the input
+	string is a pipe symbol ('|').
+
+	Inputs
+	[t_msh *] msh: a pointer to the minishell main structure.
+	[char *] str: the input string to be processed.
+	[int *] i: a pointer to the index variable.
+
+	Outputs
+	none. the function modifies the minishell structure by adding tokens to the
+	msh->tokens linked list.
+*/
 static void	add_pipe_token(t_msh *msh, char *str, int *i)
 {
 	t_list	*new_node;
@@ -49,7 +82,24 @@ static void	add_pipe_token(t_msh *msh, char *str, int *i)
 		*i += 1;
 	}
 }
+/*
+	**** ALLOCATES MEMORY ****
+	memory allocated for token_str and new_node. token_str is stored in
+	new_node->data.
+	
+	Summary
+	handles single quote tokens in a given string by extracting the substring
+	between the single quotes and adding it as a new token to msh->tokens.
 
+	Inputs
+	[t_msh *] msh: a pointer to the minishell main structure.
+	[char *] str: the input string to be processed.
+	[int *] i: a pointer to the index variable.
+
+	Outputs
+	none. the function modifies the minishell structure by adding tokens to the
+	msh->tokens linked list.
+*/
 static void	handle_s_quote_token(t_msh *msh, char *str, int *i)
 {
 	int		start;
@@ -72,6 +122,24 @@ static void	handle_s_quote_token(t_msh *msh, char *str, int *i)
 	}
 }
 
+/*
+	**** ALLOCATES MEMORY ****
+	memory allocated for token_str and new_node. token_str is stored in
+	new_node->data.
+
+	Summary
+	handles double quote tokens in a given string by extracting the substring
+	between the double quotes and adding it as a new token to msh->tokens.
+
+	Inputs
+	[t_msh *] msh: a pointer to the minishell main structure.
+	[char *] str: the input string to be processed.
+	[int *] i: a pointer to the index variable.
+
+	Outputs
+	none. the function modifies the minishell structure by adding tokens to the
+	msh->tokens linked list.
+*/
 static void	handle_d_quote_token(t_msh *msh, char *str, int *i)
 {
 	int		start;
@@ -99,6 +167,23 @@ static void	handle_d_quote_token(t_msh *msh, char *str, int *i)
 	}
 }
 
+/*
+	**** ALLOCATES MEMORY ****
+	memory is allocated for linked list nodes in sub functions
+
+	Summary
+	handles meta characters in a given string by calling different helper
+	functions based on the character encountered.
+
+	Inputs
+	[t_msh *] msh: a pointer to the minishell main structure.
+	[char *] str: the input string to be processed.
+	[int *] i: a pointer to the index variable.
+
+	Outputs
+	none. the function modifies the minishell structure by adding tokens to the
+	msh->tokens linked list.
+*/
 void	handle_meta_chars(t_msh *msh, char *str, int *i)
 {
 	if (str[*i] == '<' || str[*i] == '>')
