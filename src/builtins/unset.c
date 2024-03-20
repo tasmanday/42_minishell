a573b321e036f5221021ee615af5726bbddf6219
Tasman Day <122511599+tasmanday@users.noreply.github.com>
2024-03-20 10:45:16 +1030

    created unset.c

---
+++ src/builtins/unset.c
@@ -0,0 +1,27 @@
+#include "../../inc/minishell.h"
+
+void	ft_unset(t_msh *msh)
+{
+	t_cmd	*cmd_struct;
+	t_list	*args;
+	t_dlist	*env_node;
+
+	cmd_struct = (t_cmd *)msh->cmd_queue->data;
+	if (!cmd_struct->arguments)
+		return ;
+	else
+	{
+		args = cmd_struct->arguments;
+		while (args)
+		{
+			env_node = find_envvar_node(msh->envvar, (char *)args->data);
+			if (!env_node)
+			{
+				args = args->next;
+				continue ;
+			}
+			dlst_del_node(msh->envvar, env_node, free_envv_struct);
+			args = args->next;
+		}
+	}
+}
