8734b8c115d39ac5a18709b78b65faeaea94cd1d
Tasman Day <122511599+tasmanday@users.noreply.github.com>
2024-03-24 20:28:07 +1030

    added updated cd.c

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,119 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sentry <sentry@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tday <tday@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 11:53:35 by sentry            #+#    #+#             */
-/*   Updated: 2024/03/24 10:57:49 by sentry           ###   ########.fr       */
+/*   Updated: 2024/03/24 20:25:45 by tday             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-/*
-// WORKING FT_CD (testing for handling of absolute and relative pathing)
-void ft_cd(t_msh *msh)
+static char	*get_new_path(t_cmd	*cmd_struct)
 {
-    char cwd[PATH_MAX];
-    t_cmd *cmd_struct;
-    char *path;
-
-    cmd_struct = (t_cmd *)msh->cmd_queue->data;
-    if (!cmd_struct->arguments)
-    {
-        char *home_dir = getenv("HOME");
-        if (home_dir == NULL)
-        {
-            fprintf(stderr, "cd: HOME directory not set\n");
-            return;
-        }
-        path = home_dir;
-    }
-    else
-        path = cmd_struct->arguments->data;
-    if (chdir(path) != 0)
-    {
-        perror("cd");
-        return;
-    }
-
-    if (getcwd(cwd, sizeof(cwd)) == NULL)
-    {
-        perror("getcwd");
-        return;
-    }
-    t_dlist *current = msh->envvar;
-    while (current)
-    {
-        t_envv *env_var = (t_envv *)current->data;
-        if (ft_strcmp(env_var->env_key, "PWD") == 0)
-        {
-            free(env_var->env_value);
-            env_var->env_value = ft_strdup(cwd);
-            if (env_var->env_value == NULL)
-            {
-                perror("strdup");
-                return;
-            }
-            break;
-        }
-        current = current->next;
-    }
-    printf("%s\n", cwd);
-}
-*/
-
-/*
-// WORKING CD but doesn't update old pwd
-void	ft_cd(t_msh	*msh)
-{
-	char	cwd[PATH_MAX];
-	t_cmd	*cmd_struct;
 	char	*path;
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cmd_struct->arguments)
+		path = ft_strdup(getenv("HOME"));
+	else if (ft_strcmp(cmd_struct->arguments->data, ".") == 0)
+		path = ft_strdup(cwd);
+	else if (ft_strcmp(cmd_struct->arguments->data, "..") == 0)
+		path = NULL;
+	else
+		path = ft_strdup(cmd_struct->arguments->data);
+	free(cwd);
+	return (path);
+}
+
+static void	update_env_vars(t_msh	*msh, char	*old_pwd, char	*new_pwd)
+{
 	t_dlist	*current;
-	char	*home_dir;
 	t_envv	*env_var;
 
-	cmd_struct = (t_cmd *)msh->cmd_queue->data;
-	if (!cmd_struct->arguments)
-	{
-		home_dir = getenv("HOME");
-		if (home_dir == NULL)
-		{
-			printf("cd: HOME directory not set\n");
-			return ;
-		}
-		path = ft_strdup(home_dir);
-		if (path == NULL)
-		{
-			perror("strdup");
-			return ;
-		}
-	}
-	else
-	{
-		path = ft_strdup(cmd_struct->arguments->data);
-		if (path == NULL)
-		{
-			perror("strdup");
-			return ;
-		}
-	}
-	old_pwd = getenv("PWD");
-	if (chdir(path) != 0)
-	{
-		perror("cd");
-		free(path);
-		return ;
-	}
-	if (old_pwd != NULL)
-		setenv("OLDPWD", old_pwd, 1);
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("getcwd");
-		free(path);
-		return ;
-	}
 	current = msh->envvar;
 	while (current)
 	{
@@ -123,176 +42,59 @@ void	ft_cd(t_msh	*msh)
 		if (ft_strcmp(env_var->env_key, "PWD") == 0)
 		{
 			free(env_var->env_value);
-			env_var->env_value = ft_strdup(cwd);
-			if (env_var->env_value == NULL)
-			{
-				perror("strdup");
-				free(path);
-				return ;
-			}
-			break ;
+			env_var->env_value = ft_strdup(new_pwd);
 		}
 		current = current->next;
 	}
-	printf("%s\n", cwd);
-	free(path);
+	update_node_value(find_envvar_node(msh->envvar, "OLDPWD"), old_pwd);
 }
-*/
 
-/*
+static void	free_resources(char *path, char *old_pwd)
+{
+	if (path && path != NULL)
+	{
+		free(path);
+	}
+	if (old_pwd && old_pwd != NULL)
+	{
+		free(old_pwd);
+	}
+}
+
+static void	free_and_return(char	*path, char	*old_pwd)
+{
+	free_resources(path, old_pwd);
+	return ;
+}
+
 void	ft_cd(t_msh	*msh)
 {
 	char	cwd[PATH_MAX];
-	t_cmd	*cmd_struct;
+	char	*old_pwd;
+	char	*new_pwd;
 	char	*path;
-	t_dlist	*current;
-	char	*home_dir;
-	t_envv	*env_var;
 
-	cmd_struct = (t_cmd *)msh->cmd_queue->data;
-	if (!cmd_struct->arguments)
+	path = get_new_path((t_cmd *)msh->cmd_queue->data);
+	if (path && access(path, F_OK) != 0)
 	{
-		home_dir = getenv("HOME");
-		if (home_dir == NULL)
-		{
-			printf("cd: HOME directory not set\n");
-			return ;
-		}
-		path = ft_strdup(home_dir);
-		if (path == NULL)
-		{
-			perror("strdup");
-			return ;
-		}
-	}
-	else
-	{
-		path = ft_strdup(cmd_struct->arguments->data);
-		if (path == NULL)
-		{
-			perror("strdup");
-			return ;
-		}
-	}
-	old_pwd = getenv("PWD");
-	if (chdir(path) != 0)
-	{
-		perror("cd");
-		free(path);
+		perror("");
+		free_resources(path, NULL);
 		return ;
 	}
-	if (old_pwd != NULL)
-	//	setenv("OLDPWD", old_pwd, 1);
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("getcwd");
-		free(path);
+	if (!path && chdir("..") != 0)
 		return ;
-	}
-	current = msh->envvar;
-	while (current)
+	old_pwd = getcwd(NULL, 0);
+	if (!old_pwd)
+		free_and_return(path, old_pwd);
+	if (path)
 	{
-		env_var = (t_envv *)current->data;
-		if (ft_strcmp(env_var->env_key, "PWD") == 0)
-		{
-			free(env_var->env_value);
-			env_var->env_value = ft_strdup(cwd);
-			if (env_var->env_value == NULL)
-			{
-				perror("strdup");
-				free(path);
-				return ;
-			}
-			break ;
-		}
-		current = current->next;
+		if (chdir(path) != 0)
+			free_and_return(path, old_pwd);
 	}
-	printf("%s\n", cwd);
-	free(path);
-}
-*/
-
-void ft_cd(t_msh *msh)
-{
-    char cwd[PATH_MAX];
-    t_cmd *cmd_struct;
-    char *path;
-    t_dlist *current;
-    char *home_dir;
-    t_envv *env_var;
-    t_dlist *oldpwd_node = find_envvar_node(msh->envvar, "OLDPWD");
-	char *old_pwd;
-	char *new_pwd;
-
-    cmd_struct = (t_cmd *)msh->cmd_queue->data;
-    if (!cmd_struct->arguments)
-    {
-        home_dir = getenv("HOME");
-        if (home_dir == NULL)
-        {
-            printf("cd: HOME directory not set\n");
-            return;
-        }
-        path = ft_strdup(home_dir);
-        if (path == NULL)
-        {
-            perror("strdup");
-            return;
-        }
-    }
-    else
-    {
-        path = ft_strdup(cmd_struct->arguments->data);
-        if (path == NULL)
-        {
-            perror("strdup");
-            return;
-        }
-    }
-    old_pwd = getcwd(NULL, 0);
-    if (old_pwd == NULL)
-    {
-        perror("getcwd");
-		free(path);
-        return;
-    }
-    if (chdir(path) != 0)
-    {
-        perror("cd");
-        free(path);
-		free(old_pwd);
-        return;
-    }
-    new_pwd = getcwd(cwd, sizeof(cwd));
-    if (new_pwd == NULL)
-    {
-        perror("getcwd");
-        free(path);
-		free(old_pwd);
-        return;
-    }
-    current = msh->envvar;
-    while (current)
-    {
-        env_var = (t_envv *)current->data;
-        if (ft_strcmp(env_var->env_key, "PWD") == 0)
-        {
-            free(env_var->env_value);
-            env_var->env_value = ft_strdup(new_pwd);
-            if (env_var->env_value == NULL)
-            {
-                perror("strdup");
-                free(path);
-                return;
-            }
-        }
-        current = current->next;
-    }
-    if (oldpwd_node)
-    {
-        update_node_value(oldpwd_node, old_pwd);
-    }
-    printf("%s\n", cwd);
-    free(path);
-	free(old_pwd);
+	new_pwd = getcwd(cwd, sizeof(cwd));
+	if (!new_pwd)
+		free_and_return(path, old_pwd);
+	update_env_vars(msh, old_pwd, new_pwd);
+	//printf("%s\n", cwd);
+	free_resources(path, old_pwd);
 }
